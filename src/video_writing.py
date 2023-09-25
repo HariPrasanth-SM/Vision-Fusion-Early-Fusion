880c630205ae6fd2012ae0a2a51412b8de695fd8
Hari Prasanth SM <sm.hariprasanth@gmail.com>
2023-09-25 17:51:40 +0300

    GIF creation added

--- src/video_writing.py
+++ src/video_writing.py
@@ -3,6 +3,7 @@ import numpy as np
 import cv2
 import glob
 import tqdm
+import imageio
 
 from lidar_to_camera_projection import LiDARtoCamera
 
@@ -25,12 +26,17 @@ if __name__ == "__main__":
     ## Create a progress bar
     pbar = tqdm.tqdm(total=len(scenario_images), position=0, leave=True)
     
+    image_list = list()
     for im, pcd in zip(scenario_images, scenario_points):
         image = cv2.cvtColor(cv2.imread(im), cv2.COLOR_BGR2RGB)
         points = np.asarray(o3d.io.read_point_cloud(pcd).points)
         processed_image = lidar2camera.show_pcd_on_image(image, points)
+        image_list.append(processed_image)
         output_handle.write(cv2.cvtColor(processed_image, cv2.COLOR_BGR2RGB))
         pbar.update(1)
 
     output_handle.release()
+
+    ## Write a GIF
+    imageio.mimsave("../output/3d_to_2d_projection.gif", image_list, duration=5)
 
